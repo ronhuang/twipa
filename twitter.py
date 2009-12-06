982d001e89adf9636e863796ad39119c3efee108
Ron Huang <ronhuang@gmail.com>
2009-12-07 02:40:45 +0800

    Implemented fetching profile from Twitter.

--- twitter.py
+++ twitter.py
@@ -30,6 +30,14 @@ from google.appengine.api.labs import taskqueue
 from google.appengine.ext import db
 from django.utils import simplejson
 from google.appengine.api import urlfetch
+import datetime
+
+
+_TWITTER_PROPERTIES = frozenset(['id', 'name', 'screen_name', 'created_at',
+                                 'location', 'description', 'url',
+                                 'followers_count', 'friends_count',
+                                 'favourites_count', 'statuses_count',
+                                 'profile_image_url'])
 
 
 class Image(db.Model):
@@ -44,30 +52,129 @@ class Profile(db.Model):
   name = db.StringProperty(required=True)
   screen_name = db.StringProperty(required=True)
   created_at = db.DateTimeProperty(required=True)
-  location = db.StringProperty()
-  description = db.StringProperty()
-  url = db.LinkProperty()
+  location = db.StringProperty(default="")
+  description = db.StringProperty(default="")
+  url = db.StringProperty(default="")
   followers_count = db.IntegerProperty(required=True)
   friends_count = db.IntegerProperty(required=True)
   favourites_count = db.IntegerProperty(required=True)
   statuses_count = db.IntegerProperty(required=True)
-  image_url = db.LinkProperty()
+  profile_image_url = db.LinkProperty()
 
   # My properties.
   image = db.ReferenceProperty(Image)
-  modified_at = db.DateTimeProperty(required=True)
+  modified_at = db.DateTimeProperty()
+
+
+  def __cmp__(self, other):
+    # Compare Twitter's properties only.
+    # Profile without modified_at field is always newer.
+    diff = 0
+
+    for p in _TWITTER_PROPERTIES:
+      if self[p] != other[p]:
+        diff += 1
+
+    if other.modified_at is None or other.modified_at >= self.modified_at:
+      return diff
+    else:
+      return -diff
+
+
+def create_profile_from_json(raw):
+  data = simplejson.loads(raw)
+
+  profile = Profile(
+    id = data['id'],
+    name = data['name'],
+    screen_name = data['screen_name'],
+    # Sat Dec 05 22:32:04 +0000 2009
+    created_at = datetime.datetime.strptime(data['created_at'], "%a %b %d %H:%M:%S +0000 %Y"),
+    location = data['location'],
+    description = data['description'],
+    url = data['url'],
+    followers_count = data['followers_count'],
+    friends_count = data['friends_count'],
+    favourites_count = data['favourites_count'],
+    statuses_count = data['statuses_count'],
+    profile_image_url = db.Link(data['profile_image_url']),
+    )
+
+  return profile
+
+
+def create_image_from_normal_url(normal):
+  ext = normal[len(normal)-4:] # assuming the extension has only 3 characters
+  base = normal[:len(normal)-len('_normal.xxx')]
+
+  original = base + ext
+  bigger = base + '_bigger' + ext
+
+  result = urlfetch.fetch(normal)
+  if result.status_code != 200:
+    return None
+  normal_blob = db.Blob(result.content)
+
+  result = urlfetch.fetch(original)
+  if result.status_code != 200:
+    return None
+  original_blob = db.Blob(result.content)
+
+  result = urlfetch.fetch(bigger)
+  if result.status_code != 200:
+    return None
+  bigger_blob = db.Blob(result.content)
+
+  return Image(
+    original = original_blob,
+    normal = normal_blob,
+    bigger = bigger_blob,
+    )
 
 
 class UserHandler(webapp.RequestHandler):
 
   def post(self):
-    pass
+    id = self.request.get('id')
+    url = "http://twitter.com/users/show.json?id=%s" % (id)
+
+    result = urlfetch.fetch(url)
+    if result.status_code != 200:
+      # TODO: error handling.
+      return
+
+    remote_profile = create_profile_from_json(result.content)
+
+    query = Profile.gql("WHERE id = :id "
+                        "ORDER BY modified_at DESC",
+                        id=id)
+    local_profile = query.get()
+
+    if local_profile is None or local_profile != remote_profile:
+      # Populate image reference
+      image = create_image_from_normal_url(remote_profile.profile_image_url)
+      image.put()
+
+      remote_profile.image = image
+      remote_profile.modified_at = datetime.datetime.utcnow()
+      remote_profile.put()
 
 
 class FriendHandler(webapp.RequestHandler):
 
   def post(self):
-    pass
+    id = self.request.get('id')
+    url = "http://twitter.com/friends/ids/%s.json" % (id)
+
+    result = urlfetch.fetch(url)
+    if result.status_code != 200:
+      # TODO: error handling.
+      return
+
+    friends = simplejson.loads(result.content)
+    q = taskqueue.Queue('query-status')
+    for f in friends:
+      q.add(taskqueue.Task(url='/twitter/get-user-status', params={'id': f}))
 
 
 def main():
